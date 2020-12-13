33f4d8089e63a72ecdf6940749d3748ae15ca49b
Reese Jones <reesealanj@gwu.edu>
2020-12-12 19:31:56 -0500

    Adjusted layout of project away from server-client

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const app = express();
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Reese's URL shortener",
+    });
+});
+
+app.get("/:id", (req, res) => {
+    // TODO Redirect to URL
+});
+
+app.post("/url", (req, res) => {
+    // TODO Create Shortened URL
+});
+
+app.get("/url/:id", (req, res) => {
+    // TODO get information regarding a url
+});
+
+const port = process.env.PORT || 4848;
+app.listen(port, () => {
+    console.log(`Listening on Port: ${port}`);
+});
